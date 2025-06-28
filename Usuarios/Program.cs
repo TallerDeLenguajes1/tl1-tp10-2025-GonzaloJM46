78714064cea16c42ee903827c05127383f414fcf
GonzaJM <gonzalojamardotuc@gmail.com>
2025-06-28 12:30:30 -0300

    ej 2

---
+++ Usuarios/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using espacioUsuario;
+using System.Text.Json;
+
+
+HttpClient client = new HttpClient();
+
+
+
+HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/users");
+response.EnsureSuccessStatusCode();
+
+string responseBody = await response.Content.ReadAsStringAsync();
+
+List<Usuario> listaUsuario = JsonSerializer.Deserialize<List<Usuario>>(responseBody);
+
+int contador = 0;
+
+foreach (var usuario in listaUsuario)
+{
+    Console.WriteLine($"Nombre: {usuario.Name}");
+    Console.WriteLine($"Username: {usuario.Username}");
+    Console.WriteLine($"Email: {usuario.Email}");
+    Console.WriteLine($"Domicilio: {usuario.Address.Street} | {usuario.Address.Suite} | {usuario.Address.City}");
+
+
+    contador++;
+    if (contador >= 5)
+        break;
+}
+
+
+string jsonString = JsonSerializer.Serialize<List<Usuario>>(listaUsuario);
+
+
+File.WriteAllText("usuarios.json", jsonString);
